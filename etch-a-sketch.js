cfae65df8c7ce7c63bf1e29bc715a8438651dbe4
kybidium <bogsredeludi@gmail.com>
2023-08-18 21:41:30 -0500

    fix square sizing
    fixed the sizing of squares, accurately accounting for double-counting of border width; also rephrased button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,22 @@
 const divMain = document.querySelector('#main-div');
 
-divMain.style.display = 'flex';
-divMain.style.width = 'auto';
-divMain.style.height = '100%';
-divMain.style['flex-wrap'] = 'wrap';
-divMain.style.margin = '0 auto';
 
 let gridNum = 16;
 let divTemp;
 for (let i = 0; i < gridNum*gridNum; i++) {
     divTemp = document.createElement('div');
     divTemp.id = `div${i+1}`;
-    divTemp.style["border-width"] = '0.5px';
-    divTemp.style["border-color"] = 'black';
     divTemp.style.border = 'solid';
-    divTemp.style.flex = '1 0 0';
+    divTemp.style["border-width"] = '0.1px';
+    divTemp.style["border-color"] = 'black';
+    divTemp.style.flex = '1 1 0';
 
     //set values proportional to the grid's dimensions
     //value 2.2 chosen to divide the padding value by 2
-    //.2 arbitrarily chosen to account for border width 
-    divTemp.style.padding = `${((100/gridNum)/2.2)}%`;
-    
+    //.2 arbitrarily chosen to account for border width
+    //maybe add constant for 600px dimensions 
+    divTemp.style.padding = `${(((600-(2*gridNum))/gridNum)/2)}px`;
+    divTemp.style['background-color'] = 'white';
     divTemp.addEventListener('mouseover', (event) => {
         event.target.style['background-color'] = 'black';
     });
