ddd61e6afd7ed8fa294ce672def5d1d73051d274
kybidium <bogsredeludi@gmail.com>
2023-08-18 22:13:56 -0500

    cleaner code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,5 @@
 const divMain = document.querySelector('#main-div');
-
+const GRID_DIM = 600;
 
 let divTemp;
 
@@ -11,7 +11,7 @@ function fillDiv(gridNum = 16) {
             divMain.removeChild(element);
         });
     }
-    console.log(divMain.querySelectorAll('#main-div > div'))
+
     for (let i = 0; i < gridNum*gridNum; i++) {
         divTemp = document.createElement('div');
         divTemp.id = `div${i+1}`;
@@ -21,18 +21,17 @@ function fillDiv(gridNum = 16) {
         divTemp.style.flex = '1 1 0';
 
         //set values proportional to the grid's dimensions
-        //value 2.2 chosen to divide the padding value by 2
-        //.2 arbitrarily chosen to account for border width
-        //maybe add constant for 600px dimensions 
-        divTemp.style.padding = `${(((600-(2*gridNum))/gridNum)/2)}px`;
+        divTemp.style.padding = `${(((GRID_DIM-(2*gridNum))/gridNum)/2)}px`;
         divTemp.style['background-color'] = 'white';
+        
+        //add hover effect
         divTemp.addEventListener('mouseover', (event) => {
             event.target.style['background-color'] = 'black';
         });
 
         divMain.appendChild(divTemp);
     }
-    console.log(divMain.querySelectorAll('#main-div > div'))
+
 }
 
 const buttonMain = document.querySelector('#sizing');
@@ -49,5 +48,4 @@ buttonMain.addEventListener('click', () => {
 });
 
 
-fillDiv();
-console.log(divMain.querySelectorAll('#main-div > div'))
+fillDiv()
